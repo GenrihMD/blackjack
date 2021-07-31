8c774762821e06464f2989b05a6121aaea50e833
Лямин <genrih.md@gmail.com>
2021-07-31 16:31:23 +0300

    Add card

---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Card extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="sass">
+
+</style>
